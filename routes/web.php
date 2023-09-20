31a76560273946b82ede568b71e20a95ce1436f4
C70Schwager <cyrillgoesweblop@protonmail.com>
2023-09-20 13:44:29 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('tweet', function () {
+    return 'Das ist meine erste Route!';
+});
+
+Route::get('tweets', function () {
+    return view('tweets.index');
+});
+
